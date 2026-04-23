9ce5f338dd890dca0d65db84a6f1e82867181bbd
dibyendukhamaru200-code <dibyendukhamaru200@gmail.com>
2026-04-23 10:20:24 +0530

    Update DataProcessor.java

--- DataProcessor.java
+++ DataProcessor.java
@@ -12,7 +12,7 @@ class DataProcessor {
 }
 
 // Main class
-public class Assignment2 {
+public class Main {
     public static void main(String[] args) {
 
         DataProcessor dp = new DataProcessor();
